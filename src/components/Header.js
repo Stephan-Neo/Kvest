061f46a707ad2da059240e9b4307751e124484e3
Stephan-Neo <stepan.kazancev.04@bk.ru>
2022-03-08 10:56:42 +0600

    add rotatr 3d model

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,35 @@ import { Suspense } from 'react'
 import { OrbitControls } from '@react-three/drei';
 import Model from './model/Scene'
 import s from './styles/Header.module.css';
+import { SpotLightHelper } from 'three';
+
+const AnimateFrame = (props) => {
+    useFrame(({ clock }) => {
+      props.meshRef.current.rotation.y -= 0.01;
+    });
+    return null;
+  }
 
 function Header() {
+    const myMesh = React.useRef();
     return (
      <div className={s.preview}>
          <div className={s.greeting}>Hello</div>
          <div className={s.graphics}>
-            <Canvas camera={{position: [15, 19, 15], zoom: 1}}>
-                <OrbitControls />
+           
+            <Canvas camera={{position: [18, 10, 20], zoom: 1}}>
                 {/* <color attach='background' args={["lightblue"]}/> */}
-                <ambientLight intensity={0.5}/>
-                <spotLight position={[15, 15, 10]} angle={0.8} />
-                <Suspense fallback={null}>
-                    <Model />
-                </Suspense>
+                
+                    <OrbitControls />
+                    <ambientLight intensity={0.5}/>
+                    <spotLight position={[20, 5, -10]} angle={0.9} />
+                    
+                    <Suspense fallback={null}>
+                        <mesh ref={myMesh}>
+                            <Model />
+                        </mesh>
+                    </Suspense>
+                <AnimateFrame meshRef={myMesh} />
             </Canvas>
         </div>
     </div> 
